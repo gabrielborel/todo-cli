950c9a3cc21bbc353d019da564f2df8546071b42
Gabriel Borel <biel_borel@hotmail.com>
2023-01-29 01:22:14 -0300

    complete todo flag

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -14,6 +14,8 @@ const (
 
 func main() {
 	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+
 	flag.Parse()
 
 	todos := &todo.Todos{}
@@ -27,6 +29,18 @@ func main() {
 	case *add:
 		todos.Add("Any task")
 		err := todos.Store(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		err = todos.Store(file)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
